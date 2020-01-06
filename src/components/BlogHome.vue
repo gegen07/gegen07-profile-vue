4a3b86babe949e97830d1b6c44d46c1a51a1def4
Germano Barcelos <gegenbarcelos@gmail.com>
2020-01-06 16:02:51 -0300

    Visualizing better the blog home

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -1,64 +1,72 @@
 <template>
   <v-content>
-    <v-container>
-      <div id="blog-home">
-        <h1>{{  }}</h1>
+    <v-container justify-center grid-list-xs fill-height>
+      <v-layout align-center justify-center row wrap>
+        <h1> Opps... Blog? </h1>
         <!-- Cria `v-for` e aplica um `key`, usando uma combinação do slug e index. -->
-        <div
+        <v-flex xs12
           v-for="(post,index) in posts"
           :key="post.slug + '_' + index">
           <router-link :to="'/blog/' + post.slug">
             <v-card
               class="mx-auto"
-              max-width="400">
+              max-width="600"
+              :elevation="6">
                 <!-- Vincula results usando um `:` -->
                 <!-- Usa um `v-if`/`else` caso seja `featured_image` -->
-                <v-img
-                  v-if="post.featured_image"
-                  :src="post.featured_image"
-                  alt="">
-                </v-img>
+                <v-container>
+                  <v-img
+                    v-if="post.featured_image"
+                    :src="post.featured_image"
+                    max-height="200"
+                    alt="">
+                  </v-img>
+                </v-container>
                 <!-- <v-img
                   v-else
                   src="http://via.placeholder.com/250x250"
                   alt=""> -->
-              <v-card-title>{{ post.title }}</v-card-title>
-              <v-card-text class="text--primary">{{ post.summary }}</v-card-text>
+                  <v-card-title class="headline">{{ post.title }}</v-card-title>
+              <v-card-text class="subtitle-1 font-weight-regular text--primary">{{ post.summary }}</v-card-text>
             </v-card>
           </router-link>
-        </div>
-      </div>
+        </v-flex>
+      </v-layout>
     </v-container>
   </v-content>
 </template>
 
 <script>
-  import { butter } from '@/buttercms'
+import { butter } from '@/buttercms'
 
-  export default {
-    name: 'blog-home',
-    data() {
-      return {
-        page_title: 'Blog',
-        posts: []
-      }
-    },
-    methods: {
-      getPosts() {
-        butter.post.list({
-          page: 1,
-          page_size: 10
-        }).then(res => {
-          this.posts = res.data.data
-        })
-      }
-    },
-    created() {
-      this.getPosts()
+export default {
+  name: 'blog-home',
+  data () {
+    return {
+      page_title: 'Blog',
+      posts: []
     }
+  },
+  methods: {
+    getPosts () {
+      butter.post.list({
+        page: 1,
+        page_size: 10
+      }).then(res => {
+        this.posts = res.data.data
+      })
+    }
+  },
+  created () {
+    this.getPosts()
   }
+}
 </script>
 
 <style>
+
+a {
+    text-decoration: none
+}
 
 </style>
--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,13 +1,76 @@
 <template>
-  
+  <v-content>
+    <v-container>
+      <div id="blog-post">
+        <h1 class="font-title">{{ post.data.title }}</h1>
+        <p>Posted by {{ post.data.author.first_name }} {{ post.data.author.last_name }} on {{ parseDate(post.data.published) }} </p>
+        <div v-html="post.data.body"></div>
+        <router-link
+          v-if="post.meta.previous_post"
+          :to="/blog/ + post.meta.previous_post.slug"
+          class="button">
+          {{ post.meta.previous_post.title }}
+        </router-link>
+        <router-link
+          v-if="post.meta.next_post"
+          :to="/blog/ + post.meta.next_post.slug"
+          class="button">
+          {{ post.meta.next_post.title }}
+        </router-link>
+      </div>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
+import { butter } from '@/buttercms'
 export default {
-
+  name: 'blog-post',
+  data () {
+    return {
+      post: {}
+    }
+  },
+  methods: {
+    getPost () {
+      butter.post.retrieve(this.$route.params.slug)
+        .then(res => {
+          this.post = res.data
+        }).catch(res => {
+          console.log(res)
+        })
+    },
+    parseDate: function (date) {
+      var dateParsed = new Date(date).toLocaleDateString('pt-BR', {
+        day: 'numeric',
+        month: 'short',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+      })
+      return dateParsed
+    }
+  },
+  watch: {
+    $route: {
+      immediate: true,
+      handler (to, from) {
+        this.getPost()
+      }
+    }
+  },
+  created () {
+    this.getPost()
+  }
 }
 </script>
 
 <style>
+
+@import url('https://fonts.googleapis.com/css?family=Lato:900&display=swap')
+
+.font-title {
+  font-family: 'Lato', sans-serif
+}
 
 </style>
